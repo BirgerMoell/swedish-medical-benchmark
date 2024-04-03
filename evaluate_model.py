ee584ed568d430b9a2ab76769d9f17af1777f24b
Birger Moell <birger.moell@gmail.com>
2024-04-03 22:16:52 +0200

    First version benchmark

--- evaluate_model.py
+++ evaluate_model.py
@@ -7,17 +7,77 @@ model = "birgermoell/eir"
 from transformers import AutoTokenizer
 import transformers
 import torch
+import json
 
-messages = [{"role": "user", "content": "Är anorektal endosonografi värdefull vid dyschezi?"}]
+# load in the json file
+json_benchmark_file  = "benchmarks/pubmedqa/data/ori_pqal_swe.json"
 
-tokenizer = AutoTokenizer.from_pretrained(model)
-prompt = tokenizer.apply_chat_template(messages, tokenize=False, add_generation_prompt=True)
-pipeline = transformers.pipeline(
-    "text-generation",
-    model=model,
-    torch_dtype=torch.float16,
-    device_map="auto",
-)
+# load in the json file in python
 
-outputs = pipeline(prompt, max_new_tokens=256, do_sample=True, temperature=0.7, top_k=50, top_p=0.95)
-print(outputs[0]["generated_text"])
+def load_json_file(json_file):
+    with open(json_file) as f:
+        data = json.load(f)
+    return data
+
+data = load_json_file(json_benchmark_file)
+# print(data)
+
+def evaluate_model(data, model, system_prompt):
+
+
+    tokenizer = AutoTokenizer.from_pretrained(model)
+    pipeline = transformers.pipeline(
+        "text-generation",
+        model=model,
+        torch_dtype=torch.float16,
+        device_map="auto",
+    )
+
+    
+
+    for key, value in data.items():
+        print(f"ID: {key}")
+        print(f"ID: {key}")
+        print(f"QUESTION: {value['QUESTION']}")
+        question = value['QUESTION']
+
+        messages = [
+            {"role": "system", "content": system_prompt},
+            {"role": "user", "content": question}]
+
+        prompt = tokenizer.apply_chat_template(messages, tokenize=False, add_generation_prompt=True)
+        outputs = pipeline(prompt, max_new_tokens=256, do_sample=True, temperature=0.7, top_k=50, top_p=0.95)
+        print(outputs[0]["generated_text"])
+        answer = outputs[0]["generated_text"]
+        print("ANSWER: ", answer)
+
+        # print("CONTEXTS:")
+        # for context in value['CONTEXTS']:
+        #     print(f"- {context}")
+        print(f"FINAL DECISION: {value['final_decision']}\n")
+
+        
+
+
+
+    
+
+
+
+# messages = [{"role": "user", "content": "Är anorektal endosonografi värdefull vid dyschezi?"}]
+
+# tokenizer = AutoTokenizer.from_pretrained(model)
+# prompt = tokenizer.apply_chat_template(messages, tokenize=False, add_generation_prompt=True)
+# pipeline = transformers.pipeline(
+#     "text-generation",
+#     model=model,
+#     torch_dtype=torch.float16,
+#     device_map="auto",
+# )
+
+# outputs = pipeline(prompt, max_new_tokens=256, do_sample=True, temperature=0.7, top_k=50, top_p=0.95)
+# print(outputs[0]["generated_text"])
+
+system_prompt = "Svara på följande medicinska fråga med en av följande svarsalternativ: Ja, Nej, Kanske"
+
+evaluate_model(data, model)
